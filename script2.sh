e28bd2012eff9e9e58c0fb56afcf3e6e3f826c4a
SB2502 <sbergmans87@gmail.com>
2014-10-01 11:34:31 +0200

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -2,9 +2,9 @@
 cat nytimes_humanities_tabbed.csv | awk -F '\t' '{print int($1/10)*10,$6}' | grep 'The New York Times' | sort | uniq -c | awk '{print $1 "\t" $2}' > artikelen_per_decennium.txt
 
 # Print number of Front-page-articles with New York Times as a source (the dataset only contains Front-page-articles from the New York Times), and give a total count per decade.  Paste results into a text file.
-cat nytimes_humanities_tabbed.csv | sed -E 's/([0-9]{4})-/\1'$'\t''\1-/'  | awk '{print int($1/10)*10,$3}' | grep -i front | sort | uniq -c | awk '{print $1}' > voorpagina_artikelen_per_decennium.txt
+cat nytimes_humanities_tabbed.csv | awk -F '\t' '{if ($2 ~ "Front Page" || $8 ~ "Front Page") print int($1/10)*10}' | sort | uniq -c | awk '{print $1}' > voorpagina_artikelen.txt
 
-# Show and explanation of output:
+# Show an explanation of output (create legenda):
 echo -e "art\tdec\tfront_art"
 
 # Create output by pasting the previously created text files
