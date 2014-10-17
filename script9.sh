c47ff72415dec8faf8515ba8cd05dd8b81589336
SB2502 <sbergmans87@gmail.com>
2014-10-17 12:16:02 +0200

    Update script9.sh

--- script9.sh
+++ script9.sh
@@ -1,12 +1,6 @@
 # Desfription (added later): this script is meant to do the same thing as script 7 & 8 combined, but via another way, 
 # using programs like JQ, tree and parallel. It was meant to be faster and more easy to adapt to a different research question.
 
-
-
-
-# WHHHAAAAA {1} {2} ::: {Hulk} ::: {mode} 
-# let's hammer the effin nyt server, ban me mofo's...
-
 # 1: install parallell -> Terminal: brew install parallel
 # then  automate the request with parallel
 parallel -j2 --progress --delay 0.01 --results effinresults "curl -sL "\
